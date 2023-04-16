e4bcad0ad9d1997a2a303deee0afd3839c6c15fe
Roxana Bita <roxana.bita@gmail.com>
2023-04-16 19:32:14 +0200

    innerText and value of ref

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+//how to access a value from an input and store it with refs
+
 var one = new Vue({
     el: "#vue-app",
     data: {
         title: "App one",
+        output: "Your fav food"
     },
     methods: {
         readRefs: function () {
-            console.log(this.$refs.input.value);
+          console.log(this.$refs.test.innerText)
+            this.output=this.$refs.input.value;
         },
     },
     computed: {},
