dec49ed400ece06062e58046ab4218c4f1639e3e
Roxana Bita <roxana.bita@gmail.com>
2023-04-16 19:23:22 +0200

    get value with ref

--- app.js
+++ app.js
@@ -1,49 +1,12 @@
-//notiz for test cases:
-//row is made of test case name which receives ruleid as a  prop, and inside row is dialog component which receives test-case as prop. in each component there is a copy of the item. https://vueschool.io/lessons/reusable-components-with-props
-
-Vue.component("greeting", {
-    template:
-        '<div><p>This greeting template prints the name {{name}} </p><button v-on:click="changeName">change name</button></div>',
-    data: function () {
-        return {
-            name: "Yoshi",
-        };
-    },
-    methods: {
-        changeName: function () {
-            this.name = "Mario";
-        },
-    },
-});
-
 var one = new Vue({
-    el: "#vue-app-one",
+    el: "#vue-app",
     data: {
         title: "App one",
     },
-    methods: {},
-    computed: {
-        greet: function () {
-            return "hello from app one";
-        },
-    },
-});
-
-var two = new Vue({
-    el: "#vue-app-two",
-    data: {
-        title: "app two",
-    },
     methods: {
-        changeTitle: function () {
-            one.title = "title changed";
+        readRefs: function () {
+            console.log(this.$refs.input.value);
         },
     },
-    computed: {
-        greet: function () {
-            return "this is app 2";
-        },
-    },
+    computed: {},
 });
-
-two.title = "changed from outside vue instance";
